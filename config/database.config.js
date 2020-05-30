eccb51970a23dd66f74fe68d80d25b3b6db97d68
christian mpimbaza <xtianm4@gmail.com>
2020-05-31 01:44:07 +0300

    naledi backend

---
+++ config/database.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url: 'mongodb+srv://autosoftAdmin:20%40Krish%231@cluster0-yu1qu.mongodb.net/test?retryWrites=true&w=majority'
+}
+
+//Localhost connection
+// module.exports = {
+//     url: 'mongodb://localhost:27017/easy-notes'
+// }
